0c7e7e3615f4d04286b0b8b4626e23d03d79c61c
nimi09 <nicu.mihailescu@gmail.com>
2015-01-26 10:41:54 +0100

    adding totalrows fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,7 +12,12 @@ class StaticPagesController < ApplicationController
 	end
 
 	def display_row_count
+#       Old custom Method => probably not 100% accurate
 		@rowcount = User.count + Project.count + AssignedProject.count + Expense.count + Rental.count + Workinghour.count + Revenue.count + Feedback.count
+
+#       New Method => probaly the correct way to do it
+        st=0; ActiveRecord::Base.send(:subclasses).each {|sc| begin; st += sc.all.size unless sc.all.nil?; rescue ActiveRecord::StatementInvalid; end; };
+        @trows = st.to_s;
 	end
 
 	private
